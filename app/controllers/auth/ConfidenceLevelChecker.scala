0b19c9fe5ad83a6623c26d06b4464548b9b5e9f3
Richard Bacon <richard.bacon1@hmrc.gsi.gov.uk>
2018-01-04 11:25:34 +0000

    MTA-3067 Changed Apache License year from 2017 to 2018

--- app/controllers/auth/ConfidenceLevelChecker.scala
+++ app/controllers/auth/ConfidenceLevelChecker.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,4 +22,3 @@ import uk.gov.hmrc.play.frontend.auth.connectors.domain.{ConfidenceLevel, Creden
 trait ConfidenceLevelChecker {
   def userHasHighConfidenceLevel(implicit authContext: AuthContext) = authContext.user.confidenceLevel >= ConfidenceLevel.L200
 }
-
--- app/error/LocalErrorHandler.scala
+++ app/error/LocalErrorHandler.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -63,4 +63,3 @@ class LocalErrorHandler @Inject() (
     onClientError(request, INTERNAL_SERVER_ERROR, "")
 
 }
-
