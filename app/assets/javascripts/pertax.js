e10573cd8da30cb716f0871fe955e81419c5ade6
Adam Fenwick <adam.fenwick@digital.hmrc.gov.uk>
2018-02-06 13:06:05 +0000

    MTA-3140 Organising sass files into partials/modules and cleaning up style that isnt being used

--- app/assets/javascripts/pertax.js
+++ app/assets/javascripts/pertax.js
@@ -1,31 +1,3 @@
-$('input[name=beforeUsingYourPersonalTaxAccount]').click(function() {
-  var targetFieldsets = $('fieldset[class*=beforeUsingYourPersonalTaxAccount');
-  targetFieldsets.addClass('hidden');
-  $('label', targetFieldsets).removeClass('selected');
-  $('input', targetFieldsets).prop('checked', false);
-  $('fieldset.beforeUsingYourPersonalTaxAccount-'+$(this).val()).removeClass('hidden');
-});
-
-$('[class*="checkboxgroup-clear"]').on('change', function() {
-  if($(this).is(':checked')) {
-    var classes = $(this).attr('class').split(' ');
-    for (i in classes) {
-      var c = classes[i];
-      if ( c.startsWith('checkboxgroup-clear') )
-        $('.'+c.replace('-clear', '')+':checked').prop('checked', false).trigger('change');
-    }
-  }
-});
-
-$('[class*="visibility-toggle"]').on('change', function() {
-  var classes = $(this).attr('class').split(' ');
-  for (i in classes) {
-    var c = classes[i];
-    if ( c.startsWith('visibility-toggle') )
-      $('.'+c.replace('visibility-toggle-', '')).toggleClass('js-hidden').find('input').val('');
-  }
-});
-
 $(".full-width-banner__close").click(function(){
   $.ajax({
     url: "/personal-account/dismiss-ur-banner",
