f2a6dd7ef507f3075635a48d02e11b3493961b05
Richard Bacon <richardjamesbaconhmrc@gmail.com>
2018-08-14 10:09:56 +0100

    DDCLNS-4393 Initial spike for pdf save as button

---
+++ app/connectors/PdfGeneratorConnector.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import com.google.inject.ImplementedBy
+import javax.inject.{Inject, Singleton}
+import play.api.libs.ws.{WSClient, WSResponse}
+import services.http.WsAllMethods
+import uk.gov.hmrc.play.config.ServicesConfig
+
+import scala.concurrent.Future
+
+@ImplementedBy(classOf[FrontendPdfGeneratorConnector])
+trait PdfGeneratorConnector {
+  val serviceURL: String
+  def getWsClient: WSClient
+
+  def generatePdf(html: String): Future[WSResponse] = {
+    getWsClient.url(serviceURL).post(Map("html" -> Seq(html)))
+  }
+}
+
+@Singleton
+class FrontendPdfGeneratorConnector @Inject() (wsHttp: WsAllMethods) extends PdfGeneratorConnector with ServicesConfig {
+  val pdfServiceUrl: String = baseUrl("pdf-generator-service")
+  val serviceURL = pdfServiceUrl + "/pdf-generator-service/generate"
+  def getWsClient:WSClient = wsHttp.wsClient
+}
--- app/controllers/PrintController.scala
+++ app/controllers/PrintController.scala
@@ -17,8 +17,8 @@
 package controllers
 
 import java.util.Calendar
-import javax.inject.Inject
 
+import javax.inject.Inject
 import config.ConfigDecorator
 import connectors.{FrontEndDelegationConnector, PertaxAuditConnector, PertaxAuthConnector}
 import controllers.auth.{AuthorisedActions, PertaxRegime}
@@ -27,35 +27,64 @@ import play.api.i18n.MessagesApi
 import services.partials.MessageFrontendService
 import services.{CitizenDetailsService, UserDetailsService}
 import util.LocalPartialRetriever
+import connectors.PdfGeneratorConnector
 
 import scala.concurrent.Future
 import org.joda.time.LocalDate
+import play.api.mvc.Action
+import uk.gov.hmrc.http.BadRequestException
 
-
-
-
-class PrintController @Inject() (
-                                  val messagesApi: MessagesApi,
-                                  val citizenDetailsService: CitizenDetailsService,
-                                  val userDetailsService: UserDetailsService,
-                                  val messageFrontendService: MessageFrontendService,
-                                  val delegationConnector: FrontEndDelegationConnector,
-                                  val auditConnector: PertaxAuditConnector,
-                                  val authConnector: PertaxAuthConnector,
-                                  val partialRetriever: LocalPartialRetriever,
-                                  val configDecorator: ConfigDecorator,
-                                  val pertaxRegime: PertaxRegime,
-                                  val localErrorHandler: LocalErrorHandler
-                                ) extends PertaxBaseController with AuthorisedActions {
+class PrintController @Inject() (val messagesApi: MessagesApi,
+  val citizenDetailsService: CitizenDetailsService,
+  val userDetailsService: UserDetailsService,
+  val messageFrontendService: MessageFrontendService,
+  val delegationConnector: FrontEndDelegationConnector,
+  val auditConnector: PertaxAuditConnector,
+  val authConnector: PertaxAuthConnector,
+  val partialRetriever: LocalPartialRetriever,
+  val configDecorator: ConfigDecorator,
+  val pertaxRegime: PertaxRegime,
+  val localErrorHandler: LocalErrorHandler,
+  val pdfGeneratorConnector: PdfGeneratorConnector
+  ) extends PertaxBaseController with AuthorisedActions {
 
   def printNationalInsuranceNumber = VerifiedAction(baseBreadcrumb) {
     implicit pertaxContext =>
-      enforcePersonDetails { payeAccount => personDetails =>
-
-
-        val dateString: String = LocalDate.now.toString("MM/YY")
-
-        Future.successful(Ok(views.html.print.printNationalInsuranceNumber(personDetails,dateString)))
+      enforcePersonDetails {
+        payeAccount =>
+          personDetails =>
+            val dateString: String = LocalDate.now.toString("MM/YY")
+            Future.successful(Ok(views.html.print.printNationalInsuranceNumber(personDetails,dateString)))
       }
   }
+
+  def removeScriptTags(html: String) = html.replaceAll("<script[\\s\\S]*?/script>", "")
+
+  def saveNationalInsuranceNumberLetterAsPdf = VerifiedAction(baseBreadcrumb) {
+
+    implicit pertaxContext =>
+      enforcePersonDetails {
+        payeAccount =>
+          personDetails =>
+            val dateString: String = LocalDate.now.toString("MM/YY")
+
+            val body = """<!DOCTYPE html>
+            <html>
+              <body>
+                <h1 style="color: red">My First Heading</h1>
+                <h1 style="color: red">My Second Heading</h1>
+                 <p style="color: red;">My first paragraph.</p>
+              </body>
+            </html>"""
+
+            pdfGeneratorConnector.generatePdf(removeScriptTags(body.toString)).map { response =>
+              if (response.status != OK)
+                throw new BadRequestException(response.body)
+              else
+                Ok(response.bodyAsBytes.toArray).as("application/pdf")
+                  .withHeaders("Content-Disposition" -> s"attachment; filename=result.pdf")
+            }
+      }
+  }
+
 }
