8debc048e77736352cce9a2919406234ff4ba373
Derek Gray <derek.gray@digital.hmrc.gov.uk>
2018-09-25 16:52:50 +0100

    DDCNLS-4789 | Add GA tracking for change of address journey errors

--- app/assets/javascripts/gaTracking.js
+++ app/assets/javascripts/gaTracking.js
@@ -28,6 +28,22 @@ $('.ga-track-accordion-click').click(function(event) {
   ga('send', 'event', 'accordion - ' + (expanded ? 'hide' : 'expand'), $(this).data('ga-event-action'), $(this).data('ga-event-label'));
 });
 
+$('.ga-track-error').click(function(event) {
+  var radioButtonName = $(this).data('radio-button-name');
+  var selectedAnswer = $('input[name=' + radioButtonName +']:checked').val();
+  if (selectedAnswer === undefined) {
+    ga('send', 'event', 'error - field', $(this).data('ga-event-action'), "You must select an answer");
+  }
+});
+
+$('.ga-track-radio-select').click(function(event) {
+  var radioButtonName = $(this).data('radio-button-name');
+  var selectedAnswer = $('input[name=' + radioButtonName +']:checked').val();
+  if (selectedAnswer !== undefined) {
+    ga('send', 'event', 'button - click', $(this).data('ga-event-action'), selectedAnswer);
+  }
+});
+
 function gaWithCallback(send, event, category, action, label, callback) {
   ga(send, event, category, action, label, {
     hitCallback: gaCallback
