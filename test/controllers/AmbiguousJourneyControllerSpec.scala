d7378cf102c59d85366050c2dd42316a2b1936e6
brendanstraughan <brendan.straughan@digital.hmrc.gov.uk>
2019-06-26 13:01:21 +0100

    tests passing, title and paragraph changed

--- test/controllers/AmbiguousJourneyControllerSpec.scala
+++ test/controllers/AmbiguousJourneyControllerSpec.scala
@@ -320,9 +320,9 @@ class AmbiguousJourneyControllerSpec extends BaseSpec with ViewSpec {
 
   }
 
-  "Calling AmbiguousJourneyController.processUsedUtrToRegisterChoice" should {
-    "return 200 when AmbiguousJourneyController.processUsedUtrToRegisterChoice is called" in new LocalSetupJourney {
-      val r = controller.processUsedUtrToRegisterChoice(buildFakeRequestWithAuth("GET"))
+  "Calling AmbiguousJourneyController.usedUtrToRegisterChoice" should {
+    "return 200 when AmbiguousJourneyController.usedUtrToRegisterChoice is called" in new LocalSetupJourney {
+      val r = controller.usedUtrToRegisterChoice(buildFakeRequestWithAuth("GET"))
       override lazy val getSelfAssessmentServiceResponse = AmbiguousFilerSelfAssessmentUser(SaUtr("1111111111"))
 
       status(r) shouldBe OK
