35bc1e02dd4308d82e6acebab0c13dfb7bacc5a7
muhammad <muhammad.ahmad@digital.hmrc.gov.uk>
2021-02-10 12:20:24 +0000

    javascript removed as per DAC recommendation

--- app/assets/javascripts/pertax.js
+++ app/assets/javascripts/pertax.js
@@ -13,15 +13,4 @@ $(".print-this").css('display', 'inline-block');
 
 $('.error-summary').focus();
 
-
-// ---------------------------------------------------
-// Introduce direct skip link control, to work around voiceover failing of hash links
-// https://bugs.webkit.org/show_bug.cgi?id=179011
-// https://axesslab.com/skip-links/
-// ---------------------------------------------------
-$('.skiplink').click(function(e) {
-    e.preventDefault();
-    $(':header:first').attr('tabindex', '-1').focus();
-});
-
 GOVUK.shimLinksWithButtonRole.init();
