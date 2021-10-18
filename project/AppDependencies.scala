52ed917c8d2bbcf8796a24ce0d10280f4a0dc167
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-10-18 12:51:43 +0100

    DDCNL-4316 | changed dependencies, many compilation errors

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,30 +6,31 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc"         %% "bootstrap-frontend-play-27"        % "5.1.0",
-    "uk.gov.hmrc"         %% "play-partials"            % "8.1.0-play-27",
-    "uk.gov.hmrc"         %% "url-builder"              % "3.5.0-play-27",
-    "uk.gov.hmrc"         %% "http-caching-client"      % "9.4.0-play-27",
-    "uk.gov.hmrc"         %% "play-language"            % "4.13.0-play-27",
-    "uk.gov.hmrc"         %% "local-template-renderer"  % "2.16.0-play-27",
-    "uk.gov.hmrc"         %% "play-ui"                  % "9.0.0-play-27",
-    "uk.gov.hmrc"         %% "tax-year"                 % "1.1.0",
-    "uk.gov.hmrc"         %% "time"                     % "3.19.0",
-    "uk.gov.hmrc"         %% "domain"                   % "5.11.0-play-27",
-    "org.reactivemongo"   %% "play2-reactivemongo"      % "0.18.6-play27",
-    "io.lemonlabs"        %% "scala-uri"                % "2.2.3",
-    "com.typesafe.play"   %% "play-json-joda"           % "2.6.10"
+    "uk.gov.hmrc"         %% "bootstrap-frontend-play-27" % "5.1.0",
+    "uk.gov.hmrc"         %% "play-partials"              % "8.1.0-play-27",
+    "uk.gov.hmrc"         %% "url-builder"                % "3.5.0-play-27",
+    "uk.gov.hmrc"         %% "http-caching-client"        % "9.4.0-play-27",
+    "uk.gov.hmrc"         %% "play-language"              % "4.13.0-play-27",
+    "uk.gov.hmrc"         %% "local-template-renderer"    % "2.16.0-play-27",
+    "uk.gov.hmrc"         %% "play-ui"                    % "9.0.0-play-27",
+    "uk.gov.hmrc"         %% "tax-year"                   % "1.1.0",
+    "uk.gov.hmrc"         %% "time"                       % "3.19.0",
+    "uk.gov.hmrc"         %% "domain"                     % "5.11.0-play-27",
+    "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-27"         % "0.55.0",
+    "io.lemonlabs"        %% "scala-uri"                  % "2.2.3",
+    "com.typesafe.play"   %% "play-json-joda"             % "2.6.10"
   )
 
   val test = Seq(
     "org.scalatest"           %% "scalatest"                % "3.2.8",
     "com.typesafe.play"       %% "play-test"                % current,
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-27"  % "0.55.0",
     "org.scalatestplus"       %% "mockito-3-4"              % "3.2.3.0",
     "org.mockito"             %  "mockito-core"             % "3.6.28",
     "org.scalacheck"          %% "scalacheck"               % "1.15.1",
     "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.27.2",
-    "com.vladsch.flexmark"    % "flexmark-all"              % "0.36.8"
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8"
   ).map(_ % "test,it")
 
   val all: Seq[ModuleID] = compile ++ test
