eeb88295a4f7db4dc8a9a6be5105612e74c248ec
Pascal Parois <pascal22p@parois.net>
2022-03-07 09:16:08 +0000

    DDCNL-5490: added toggle behind actual SEISS tile

--- app/controllers/controllershelpers/HomeCardGenerator.scala
+++ app/controllers/controllershelpers/HomeCardGenerator.scala
@@ -58,7 +58,7 @@ class HomeCardGenerator @Inject() (
       getTaxCalculationCard(taxCalculationStateCyMinusOne),
       getTaxCalculationCard(taxCalculationStateCyMinusTwo),
       getSelfAssessmentCard(saActionNeeded, currentTaxYear + 1),
-      if (showSeissCard && configDecorator.isSeissTileEnabled) Some(seissView()) else None,
+      if (showSeissCard && configDecorator.isSeissTileEnabled && !configDecorator.newSaItsaTileEnabled) Some(seissView()) else None,
       getNationalInsuranceCard(),
       getAnnualTaxSummaryCard
     ).flatten
