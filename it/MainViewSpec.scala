d5fee49ca2731f7d49e302c45440ec1a23ce9b38
Sachin300793 <56870052+Sachin300793@users.noreply.github.com>
2022-03-16 17:26:23 +0000

    DDCNL-5409: fixing

--- it/MainViewSpec.scala
+++ it/MainViewSpec.scala
@@ -37,7 +37,7 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import play.twirl.api.Html
 import services.{EnrolmentStoreCachingService, LocalSessionCache}
-import uk.gov.hmrc.auth.core.ConfidenceLevel
+import uk.gov.hmrc.auth.core.{ConfidenceLevel, Enrolment, EnrolmentIdentifier}
 import uk.gov.hmrc.auth.core.retrieve.v2.TrustedHelper
 import uk.gov.hmrc.auth.core.retrieve.{Credentials, Name}
 import uk.gov.hmrc.domain.{Generator, Nino, SaUtr, SaUtrGenerator}
@@ -135,6 +135,7 @@ class MainViewSpec extends AnyWordSpecLike with Matchers with GuiceOneAppPerSuit
         confidenceLevel,
         personDetails,
         trustedHelper,
+        Set(Enrolment("IR-SA", Seq(EnrolmentIdentifier("UTR", new SaUtrGenerator().nextSaUtr.utr)), "Activated")),
         profile,
         messageCount,
         None,
