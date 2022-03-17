b9f4994103a9252c0773754bdc018c131e44f9d2
Sachin300793 <56870052+Sachin300793@users.noreply.github.com>
2022-03-17 17:27:20 +0000

    DDCNL-5409: Fixing font size of heading and adding toggle check to news tile

--- app/controllers/InterstitialController.scala
+++ app/controllers/InterstitialController.scala
@@ -123,10 +123,14 @@ class InterstitialController @Inject() (
   }
 
   def displayNewsAndUpdates: Action[AnyContent] = authenticate { implicit request =>
-    Ok(
-      viewNewsAndUpdatesView(
-        redirectUrl = currentUrl
+    if (configDecorator.isNewsAndUpdatesTileEnabled) {
+      Ok(
+        viewNewsAndUpdatesView(
+          redirectUrl = currentUrl
+        )
       )
-    )
+    } else {
+      errorRenderer.error(UNAUTHORIZED)
+    }
   }
 }
