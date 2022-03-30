efb76cd192d0d36c033cf2fadfc5378e6112f8ba
Luka-Abey <luka.abeywickrama@hotmail.co.uk>
2022-03-30 15:14:23 +0100

    DDCNL-5650 | removed comments, reinstated css

--- it/testUtils/IntegrationSpec.scala
+++ it/testUtils/IntegrationSpec.scala
@@ -1,25 +1,16 @@
 package testUtils
 
 import com.github.tomakehurst.wiremock.client.WireMock.{get, ok, post, urlEqualTo, urlMatching}
-import config.ConfigDecorator
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.time.{Millis, Seconds, Span}
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api
-import play.api.Application
-import play.api.i18n.{Lang, Messages, MessagesApi, MessagesImpl, MessagesProvider}
-import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
-import repositories.EditAddressLockRepository
 import uk.gov.hmrc.domain.Generator
-import uk.gov.hmrc.play.partials.FormPartialRetriever
 import uk.gov.hmrc.renderer.TemplateRenderer
-import testUtils.MockTemplateRenderer
-
 import scala.concurrent.ExecutionContext
-import scala.reflect.ClassTag
 
 trait IntegrationSpec extends AnyWordSpec with GuiceOneAppPerSuite with WireMockHelper with ScalaFutures with Matchers {
 
@@ -84,23 +75,6 @@ trait IntegrationSpec extends AnyWordSpec with GuiceOneAppPerSuite with WireMock
       ).overrides(
       api.inject.bind[TemplateRenderer].to(testUtils.MockTemplateRenderer)
     )
-//
-//  override implicit lazy val app: Application = localGuiceApplicationBuilder().build()
-//
-//  implicit lazy val ec = app.injector.instanceOf[ExecutionContext]
-//
-//  lazy val config = app.injector.instanceOf[ConfigDecorator]
-//
-//  implicit lazy val templateRenderer = app.injector.instanceOf[TemplateRenderer]
-//
-//  def injected[T](c: Class[T]): T = app.injector.instanceOf(c)
-//
-//  def injected[T](implicit evidence: ClassTag[T]): T = app.injector.instanceOf[T]
-//
-//  implicit lazy val messageProvider = app.injector.instanceOf[MessagesProvider]
-//  lazy val messagesApi = app.injector.instanceOf[MessagesApi]
-//
-//  implicit lazy val messages: Messages = MessagesImpl(Lang("en"), messagesApi)
 
   override def beforeEach() = {
     super.beforeEach()
