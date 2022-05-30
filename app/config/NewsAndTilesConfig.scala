71c20ff6e159f92cdd55cff316a201d19d72439a
SandeepUmredkar <sandeep.umredkar@digital.hmrc.gov.uk>
2022-05-30 12:12:15 +0100

    DDCNL-5574 - FrontEndConfig

---
+++ app/config/NewsAndTilesConfig.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+import com.google.inject.{Inject, Singleton}
+import models.NewsAndContentModel
+import play.api.Configuration
+import play.api.i18n.Lang
+
+import scala.collection.JavaConverters._
+
+@Singleton
+class NewsAndTilesConfig @Inject()(configuration: Configuration) {
+
+  def getNewsAndContentModelList(lang: Lang): List[NewsAndContentModel] = {
+    configuration.underlying.getObject("feature.news").asScala.map {
+
+      case (newsSection, _) =>
+        val shortDescription = if (lang.code equals "en") {
+          configuration.get[String](s"feature.news.$newsSection.short-description-en")
+        } else {
+          configuration.get[String](s"feature.news.$newsSection.short-description-cy")
+        }
+        val content = if (lang.code equals "en") {
+          configuration.get[String](s"feature.news.$newsSection.content-en")
+        } else {
+          configuration.get[String](s"feature.news.$newsSection.content-cy")
+        }
+        val startDate = configuration.get[String](s"feature.news.$newsSection.start-date")
+        val endDate = configuration.get[String](s"feature.news.$newsSection.end-date")
+        NewsAndContentModel(shortDescription, content, startDate, endDate)
+    }.toList
+  }
+}
---
+++ app/models/NewsAndContentModel.scala
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ */
+
+package models
+
+case class NewsAndContentModel(shortDescription: String, content: String, startDate: String, endDate: String)
