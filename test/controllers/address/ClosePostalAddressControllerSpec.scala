09b376efb935d6a109b9c3e2df30781422c9062d
Nj147 <nathanjackson147@gmail.com>
2022-07-18 10:07:25 +0100

    Changed ClosePostalAddressController tests to work with nav bar behaviour

--- test/controllers/address/ClosePostalAddressControllerSpec.scala
+++ test/controllers/address/ClosePostalAddressControllerSpec.scala
@@ -49,7 +49,12 @@ class ClosePostalAddressControllerSpec extends AddressBaseSpec {
       closedPostalAddress = true,
       Some(fakeAddress.fullAddress),
       None
-    )(buildUserRequest(request = FakeRequest()), configDecorator, messages, ec).toString
+    )(
+      buildUserRequest(request = FakeRequest(), saUser = NonFilerSelfAssessmentUser),
+      configDecorator,
+      messages,
+      ec
+    ).toString
 
     def controller: ClosePostalAddressController =
       new ClosePostalAddressController(
