d9dc1d61c09075550a301f7adfebe88186c504df
Pascal Parois <pascal22p@parois.net>
2022-10-31 17:45:13 +0000

    updated hmrc-mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,6 +6,7 @@ import play.sbt.PlayImport.ehcache
 object AppDependencies {
 
   private val playVersion = "play-28"
+  private val hmrcMongoVersion = "0.73.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
@@ -16,7 +17,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "tax-year"                         % "3.0.0",
     "uk.gov.hmrc"       %% "time"                             % "3.19.0",
     "uk.gov.hmrc"       %% "domain"                           % s"8.0.0-$playVersion",
-    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"         % "0.71.0",
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"         % hmrcMongoVersion,
     "io.lemonlabs"      %% "scala-uri"                        % "2.2.3",
     "uk.gov.hmrc"       %% "play-frontend-hmrc"               % s"3.32.0-$playVersion",
     "uk.gov.hmrc"       %% "play-frontend-pta"                % "0.3.0",
@@ -31,7 +32,7 @@ object AppDependencies {
     "org.scalatest"          %% "scalatest"                     % "3.2.8",
     "com.typesafe.play"      %% "play-test"                     % current,
     "org.scalatestplus.play" %% "scalatestplus-play"            % "4.0.3",
-    "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-test-$playVersion" % "0.68.0",
+    "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion,
     "org.scalatestplus"      %% "mockito-3-4"                   % "3.2.3.0",
     "org.mockito"             % "mockito-core"                  % "3.6.28",
     "org.scalacheck"         %% "scalacheck"                    % "1.15.1",
