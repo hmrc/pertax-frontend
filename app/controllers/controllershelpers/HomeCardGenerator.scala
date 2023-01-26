fa361d343e76ba2320b45b29b1dabf072eae72ce
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2023-01-26 12:11:43 +0000

    Ddcnl 6980: Tweak self assessment urls (#680)
    * DDCNL-6980: tweak sa links and order
    
    * Removed old file

--- app/controllers/controllershelpers/HomeCardGenerator.scala
+++ app/controllers/controllershelpers/HomeCardGenerator.scala
@@ -42,7 +42,6 @@ class HomeCardGenerator @Inject() (
   featureFlagService: FeatureFlagService,
   payAsYouEarnView: PayAsYouEarnView,
   taxCalculationView: TaxCalculationView,
-  selfAssessmentView: SelfAssessmentView,
   nationalInsuranceView: NationalInsuranceView,
   taxCreditsView: TaxCreditsView,
   childBenefitView: ChildBenefitView,
