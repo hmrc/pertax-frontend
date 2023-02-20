81359a37f82ad4767218df6d1d5d9cedc129db12
askar.makkunnath <112563872+askar-makkunnath@users.noreply.github.com>
2023-02-20 08:41:57 +0000

    DDCNL-6933 : Updated bootstrap to the latest version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   private val playVersion = "play-28"
   private val hmrcMongoVersion = "0.73.0"
-  private val bootstrapVersion = "7.12.0"
+  private val bootstrapVersion = "7.13.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
