df19e5db969b89643a5552de3fd0049fdedd0cc6
Christian-QA <credfernbus@gmail.com>
2023-04-17 16:29:38 +0100

    DDCNL-7309: Added first 5 scenarios;

--- it/HomeControllerISpec.scala
+++ it/HomeControllerISpec.scala
@@ -4,10 +4,11 @@ import play.api.Application
 import play.api.http.Status._
 import play.api.libs.json.Json
 import play.api.mvc.{AnyContentAsEmpty, Result}
-import play.api.test.FakeRequest
+import play.api.test.{FakeRequest, Helpers}
 import play.api.test.Helpers.{GET, contentAsString, defaultAwaitTimeout, redirectLocation, route, writeableOf_AnyContentAsEmpty, status => httpStatus}
 import services.admin.FeatureFlagService
-import testUtils.IntegrationSpec
+import testUtils.{FileHelper, IntegrationSpec}
+import uk.gov.hmrc.domain.{Generator, SaUtr}
 import uk.gov.hmrc.http.SessionKeys
 import uk.gov.hmrc.http.cache.client.CacheMap
 
@@ -47,6 +48,8 @@ class HomeControllerISpec extends IntegrationSpec {
     FakeRequest(GET, url).withSession(SessionKeys.sessionId -> uuid, SessionKeys.authToken -> "1")
   }
 
+  val generatedUtr = new Generator().nextAtedUtr.utr
+
   implicit lazy val ec = app.injector.instanceOf[ExecutionContext]
 
   val breathingSpaceUrl = s"/$generatedNino/memorandum"
@@ -64,6 +67,8 @@ class HomeControllerISpec extends IntegrationSpec {
        |    "breathingSpaceIndicator": false
        |}
        |""".stripMargin
+
+  val uuid: String = UUID.randomUUID().toString
 
   override def beforeEach() = {
     server.resetAll()
@@ -106,6 +111,18 @@ class HomeControllerISpec extends IntegrationSpec {
       contentAsString(result).contains("/personal-account/breathing-space") mustBe true
       server.verify(1, getRequestedFor(urlEqualTo(s"/$generatedNino/memorandum")))
       server.verify(0, getRequestedFor(urlEqualTo(s"/taxcalc/$generatedNino/reconciliations")))
+
+      val urlBreathingSpace = "/personal-account/breathing-space"
+
+      val requestBreathingSpace = FakeRequest(GET, urlBreathingSpace)
+        .withSession(SessionKeys.authToken -> "Bearer 1", SessionKeys.sessionId -> s"session-$uuid")
+      val resultBreathingSpace  = route(app, requestBreathingSpace)
+
+      Helpers.status(resultBreathingSpace.get) mustBe OK
+      contentAsString(resultBreathingSpace.get) must include("You are in Breathing Space")
+      contentAsString(resultBreathingSpace.get) must include(
+        "The Debt Respite Scheme (Breathing Space) gives people in debt the right to legal protections from their creditors."
+      )
     }
 
     "hide BreathingSpaceIndicator when receive false response from BreathingSpaceIfProxy" in {
@@ -130,45 +147,148 @@ class HomeControllerISpec extends IntegrationSpec {
       server.verify(0, getRequestedFor(urlEqualTo(s"/taxcalc/$generatedNino/reconciliations")))
     }
 
-    List(
-      TOO_MANY_REQUESTS,
-      INTERNAL_SERVER_ERROR,
-      BAD_GATEWAY,
-      SERVICE_UNAVAILABLE,
-      IM_A_TEAPOT,
-      NOT_FOUND,
-      BAD_REQUEST,
-      UNPROCESSABLE_ENTITY
-    ).foreach { httpResponse =>
-      s"return a $httpResponse when $httpResponse status is received" in {
-        server.stubFor(post(urlEqualTo("/auth/authorise")).willReturn(ok(authResponse)))
-        server.stubFor(
-          get(urlEqualTo(s"/tai/$generatedNino/tax-account/${LocalDateTime.now().getYear}/tax-components"))
-            .willReturn(serverError())
-        )
-        server.stubFor(
-          put(urlMatching(s"/keystore/pertax-frontend/.*"))
-            .willReturn(ok(Json.toJson(CacheMap("id", Map.empty)).toString))
-        )
-        server.stubFor(
-          get(urlPathEqualTo(breathingSpaceUrl))
-            .willReturn(aResponse.withStatus(httpResponse))
-        )
+    "show correct message on the Marriage Allowance tile when transferring Personal Allowance to partner" in {
 
-        val result: Future[Result] = route(app, request).get
-        contentAsString(result).contains("BREATHING SPACE") mustBe false
-        contentAsString(result).contains("/personal-account/breathing-space") mustBe false
-        server.verify(1, getRequestedFor(urlEqualTo(s"/$generatedNino/memorandum")))
-        server.verify(0, getRequestedFor(urlEqualTo(s"/taxcalc/$generatedNino/reconciliations")))
-      }
+      val taxComponentsJson = Json
+        .parse("""{
+          |   "data" : [ {
+          |      "componentType" : "MarriageAllowanceTransferred",
+          |      "employmentId" : 12,
+          |      "amount" : 12321,
+          |      "inputAmount" : 12321,
+          |      "description" : "Personal Allowance transferred to partner",
+          |      "iabdCategory" : "Deduction"
+          |   } ],
+          |   "links" : [ ]
+          |}""".stripMargin)
+        .toString
+
+      server.stubFor(post(urlEqualTo("/auth/authorise")).willReturn(ok(authResponse)))
+      server.stubFor(
+        get(urlEqualTo(s"/tai/$generatedNino/tax-account/${LocalDateTime.now().getYear}/tax-components"))
+          .willReturn(ok(taxComponentsJson))
+      )
+      server.stubFor(
+        put(urlMatching("/keystore/pertax-frontend/.*"))
+          .willReturn(ok(Json.toJson(CacheMap("id", Map.empty)).toString))
+      )
+
+      val result: Future[Result] = route(app, request).get
+      httpStatus(result) mustBe OK
+
+      server.verify(
+        1,
+        getRequestedFor(urlEqualTo(s"/tai/$generatedNino/tax-account/${LocalDateTime.now().getYear}/tax-components"))
+      )
+
+      contentAsString(result).contains(
+        "You currently transfer part of your Personal Allowance to your partner."
+      ) mustBe true
+      server.verify(
+        1,
+        getRequestedFor(urlEqualTo(s"/tai/$generatedNino/tax-account/${LocalDateTime.now().getYear}/tax-components"))
+      )
     }
 
-    "redirect to protect-tax-info if HMRC-PT enrolment is not present" in {
-      val authResponseNoHmrcPt =
+    "show correct message on the Marriage Allowance tile when receiving Personal Allowance from partner" in {
+
+      val taxComponentsJson = Json
+        .parse("""{
+            |   "data" : [ {
+            |      "componentType" : "MarriageAllowanceReceived",
+            |      "employmentId" : 12,
+            |      "amount" : 12321,
+            |      "inputAmount" : 12321,
+            |      "description" : "Personal Allowance transferred to partner",
+            |      "iabdCategory" : "Deduction"
+            |   } ],
+            |   "links" : [ ]
+            |}""".stripMargin)
+        .toString
+
+      server.stubFor(post(urlEqualTo("/auth/authorise")).willReturn(ok(authResponse)))
+      server.stubFor(
+        get(urlEqualTo(s"/tai/$generatedNino/tax-account/${LocalDateTime.now().getYear}/tax-components"))
+          .willReturn(ok(taxComponentsJson))
+      )
+      server.stubFor(
+        put(urlMatching("/keystore/pertax-frontend/.*"))
+          .willReturn(ok(Json.toJson(CacheMap("id", Map.empty)).toString))
+      )
+
+      val result: Future[Result] = route(app, request).get
+      httpStatus(result) mustBe OK
+
+      contentAsString(result).contains(
+        "Your partner currently transfers part of their Personal Allowance to you."
+      ) mustBe true
+      server.verify(
+        1,
+        getRequestedFor(urlEqualTo(s"/tai/$generatedNino/tax-account/${LocalDateTime.now().getYear}/tax-components"))
+      )
+    }
+
+    "show correct message on the Marriage Allowance tile when not transferring or receiving Personal Allowance from partner" in {
+
+      val taxComponentsJson = Json
+        .parse("""{
+            |   "data" : [ {
+            |      "componentType" : "OtherAllowance",
+            |      "employmentId" : 12,
+            |      "amount" : 12321,
+            |      "inputAmount" : 12321,
+            |      "description" : "Personal Allowance transferred to partner",
+            |      "iabdCategory" : "Deduction"
+            |   } ],
+            |   "links" : [ ]
+            |}""".stripMargin)
+        .toString
+
+      server.stubFor(post(urlEqualTo("/auth/authorise")).willReturn(ok(authResponse)))
+      server.stubFor(
+        get(urlEqualTo(s"/tai/$generatedNino/tax-account/${LocalDateTime.now().getYear}/tax-components"))
+          .willReturn(ok(taxComponentsJson))
+      )
+      server.stubFor(
+        put(urlMatching("/keystore/pertax-frontend/.*"))
+          .willReturn(ok(Json.toJson(CacheMap("id", Map.empty)).toString))
+      )
+
+      val result: Future[Result] = route(app, request).get
+      httpStatus(result) mustBe OK
+
+      contentAsString(result).contains(
+        "Transfer part of your Personal Allowance to your partner so they pay less tax."
+      ) mustBe true
+      server.verify(
+        1,
+        getRequestedFor(urlEqualTo(s"/tai/$generatedNino/tax-account/${LocalDateTime.now().getYear}/tax-components"))
+      )
+    }
+
+    "Return LOCKED status and display the MCI error page when designatory-details returns LOCKED" in {
+
+      server.stubFor(post(urlEqualTo("/auth/authorise")).willReturn(ok(authResponse)))
+      server.stubFor(
+        get(urlEqualTo(s"/citizen-details/$generatedNino/designatory-details"))
+          .willReturn(aResponse().withStatus(LOCKED))
+      )
+
+      val result: Future[Result] = route(app, request).get
+      httpStatus(result) mustBe LOCKED
+
+      contentAsString(result).contains(
+        "We cannot access your details"
+      ) mustBe true
+    }
+
+    "show SaUtr when user has an SaUtr in the auth body" in {
+      val authResponse =
         s"""
            |{
            |    "confidenceLevel": 200,
            |    "nino": "$generatedNino",
+           |    "sautr": "$generatedUtr",
            |    "name": {
            |        "name": "John",
            |        "lastName": "Smith"
@@ -185,32 +305,128 @@ class HomeControllerISpec extends IntegrationSpec {
            |        "ggCredId": "xyz"
            |    },
            |    "externalId": "testExternalId",
-           |    "allEnrolments": [],
+           |    "allEnrolments": [
+           |       {
+           |          "key":"HMRC-PT",
+           |          "identifiers": [
+           |             {
+           |                "key":"NINO",
+           |                "value": "$generatedNino"
+           |             }
+           |          ]
+           |       },
+           |       "key":"IR-SA",
+           |          "identifiers": [
+           |             {
+           |                "key":"UTR",
+           |                "value": "$generatedUtr"
+           |             }
+           |          ]
+           |       },
+           |    ],
            |    "affinityGroup": "Individual",
            |    "credentialStrength": "strong"
            |}
            |""".stripMargin
 
-      server.stubFor(post(urlEqualTo("/auth/authorise")).willReturn(ok(authResponseNoHmrcPt)))
+      server.stubFor(post(urlEqualTo("/auth/authorise")).willReturn(ok(authResponse)))
+      server.stubFor(get(urlEqualTo(s"/taxcalc/$generatedNino/reconciliations")).willReturn(serverError()))
       server.stubFor(
         get(urlEqualTo(s"/tai/$generatedNino/tax-account/${LocalDateTime.now().getYear}/tax-components"))
           .willReturn(serverError())
       )
       server.stubFor(
-        put(urlMatching(s"/keystore/pertax-frontend/.*"))
+        put(urlMatching("/keystore/pertax-frontend/.*"))
           .willReturn(ok(Json.toJson(CacheMap("id", Map.empty)).toString))
-      )
-      server.stubFor(
-        get(urlPathEqualTo(breathingSpaceUrl))
-          .willReturn(ok(breathingSpaceTrueResponse))
       )
 
       val result: Future[Result] = route(app, request).get
-      httpStatus(result) mustBe SEE_OTHER
-      redirectLocation(result) mustBe Some("http://localhost:7750/protect-tax-info?redirectUrl=%2Fpersonal-account")
-      server.verify(0, getRequestedFor(urlEqualTo(s"/$generatedNino/memorandum")))
-      server.verify(0, getRequestedFor(urlEqualTo(s"/taxcalc/$generatedNino/reconciliations")))
+      httpStatus(result) mustBe OK
+      contentAsString(result).contains(generatedUtr) mustBe true
+
     }
+
+//    List(
+//      TOO_MANY_REQUESTS,
+//      INTERNAL_SERVER_ERROR,
+//      BAD_GATEWAY,
+//      SERVICE_UNAVAILABLE,
+//      IM_A_TEAPOT,
+//      NOT_FOUND,
+//      BAD_REQUEST,
+//      UNPROCESSABLE_ENTITY
+//    ).foreach { httpResponse =>
+//      s"return a $httpResponse when $httpResponse status is received" in {
+//        server.stubFor(post(urlEqualTo("/auth/authorise")).willReturn(ok(authResponse)))
+//        server.stubFor(
+//          get(urlEqualTo(s"/tai/$generatedNino/tax-account/${LocalDateTime.now().getYear}/tax-components"))
+//            .willReturn(serverError())
+//        )
+//        server.stubFor(
+//          put(urlMatching(s"/keystore/pertax-frontend/.*"))
+//            .willReturn(ok(Json.toJson(CacheMap("id", Map.empty)).toString))
+//        )
+//        server.stubFor(
+//          get(urlPathEqualTo(breathingSpaceUrl))
+//            .willReturn(aResponse.withStatus(httpResponse))
+//        )
+//
+//        val result: Future[Result] = route(app, request).get
+//        contentAsString(result).contains("BREATHING SPACE") mustBe false
+//        contentAsString(result).contains("/personal-account/breathing-space") mustBe false
+//        server.verify(1, getRequestedFor(urlEqualTo(s"/$generatedNino/memorandum")))
+//        server.verify(0, getRequestedFor(urlEqualTo(s"/taxcalc/$generatedNino/reconciliations")))
+//      }
+//    }
+//
+//    "redirect to protect-tax-info if HMRC-PT enrolment is not present" in {
+//      val authResponseNoHmrcPt =
+//        s"""
+//           |{
+//           |    "confidenceLevel": 200,
+//           |    "nino": "$generatedNino",
+//           |    "name": {
+//           |        "name": "John",
+//           |        "lastName": "Smith"
+//           |    },
+//           |    "loginTimes": {
+//           |        "currentLogin": "2021-06-07T10:52:02.594Z",
+//           |        "previousLogin": null
+//           |    },
+//           |    "optionalCredentials": {
+//           |        "providerId": "4911434741952698",
+//           |        "providerType": "GovernmentGateway"
+//           |    },
+//           |    "authProviderId": {
+//           |        "ggCredId": "xyz"
+//           |    },
+//           |    "externalId": "testExternalId",
+//           |    "allEnrolments": [],
+//           |    "affinityGroup": "Individual",
+//           |    "credentialStrength": "strong"
+//           |}
+//           |""".stripMargin
+//
+//      server.stubFor(post(urlEqualTo("/auth/authorise")).willReturn(ok(authResponseNoHmrcPt)))
+//      server.stubFor(
+//        get(urlEqualTo(s"/tai/$generatedNino/tax-account/${LocalDateTime.now().getYear}/tax-components"))
+//          .willReturn(serverError())
+//      )
+//      server.stubFor(
+//        put(urlMatching(s"/keystore/pertax-frontend/.*"))
+//          .willReturn(ok(Json.toJson(CacheMap("id", Map.empty)).toString))
+//      )
+//      server.stubFor(
+//        get(urlPathEqualTo(breathingSpaceUrl))
+//          .willReturn(ok(breathingSpaceTrueResponse))
+//      )
+//
+//      val result: Future[Result] = route(app, request).get
+//      httpStatus(result) mustBe SEE_OTHER
+//      redirectLocation(result) mustBe Some("http://localhost:7750/protect-tax-info?redirectUrl=%2Fpersonal-account")
+//      server.verify(0, getRequestedFor(urlEqualTo(s"/$generatedNino/memorandum")))
+//      server.verify(0, getRequestedFor(urlEqualTo(s"/taxcalc/$generatedNino/reconciliations")))
+//    }
 
   }
 }
