208d5c2e279cc9224d5aaa008ec8542960d34f82
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2023-10-18 11:07:38 +0100

    Ddcnl 6588 (#765)
    DDCNL-6588

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   private val playVersion = "play-28"
   private val hmrcMongoVersion = "1.3.0"
-  private val bootstrapVersion = "7.19.0"
+  private val bootstrapVersion = "7.20.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
