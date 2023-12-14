f007ecbab81d1c493c9bfd0ac69583b3b9055f97
grantrobson <grant.robson@digital.hmrc.gov.uk>
2023-12-14 07:06:21 +0000

    8107 Get rid of cyclomatic complexity error

--- app/connectors/HttpClientResponse.scala
+++ app/connectors/HttpClientResponse.scala
@@ -23,27 +23,28 @@ import play.api.http.Status.{BAD_GATEWAY, LOCKED, NOT_FOUND, TOO_MANY_REQUESTS}
 import uk.gov.hmrc.http.{HttpException, HttpResponse, UpstreamErrorResponse}
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 
 class HttpClientResponse @Inject() (implicit ec: ExecutionContext) extends Logging {
 
-  //scalastyle:off cyclomatic.complexity
+  private val logErrorResponses: PartialFunction[Try[Either[UpstreamErrorResponse, HttpResponse]], Unit] = {
+    case Success(Left(error)) if error.statusCode == NOT_FOUND                                    =>
+      logger.info(error.message)
+    case Success(Left(error)) if error.statusCode == LOCKED                                       =>
+      logger.warn(error.message)
+    case Success(Left(error)) if error.statusCode >= 499 || error.statusCode == TOO_MANY_REQUESTS =>
+      logger.error(error.message)
+    case Success(Left(error))                                                                     =>
+      logger.error(error.message, error)
+    case Failure(exception: HttpException)                                                        =>
+      logger.error(exception.message)
+  }
+
   def read(
     response: Future[Either[UpstreamErrorResponse, HttpResponse]]
   ): EitherT[Future, UpstreamErrorResponse, HttpResponse] =
     EitherT(
-      response andThen {
-        case Success(Left(error)) if error.statusCode == NOT_FOUND                                    =>
-          logger.info(error.message)
-        case Success(Left(error)) if error.statusCode == LOCKED                                       =>
-          logger.warn(error.message)
-        case Success(Left(error)) if error.statusCode >= 499 || error.statusCode == TOO_MANY_REQUESTS =>
-          logger.error(error.message)
-        case Success(Left(error))                                                                     =>
-          logger.error(error.message, error)
-        case Failure(exception: HttpException)                                                        =>
-          logger.error(exception.message)
-      } recover { case exception: HttpException =>
+      response andThen logErrorResponses recover { case exception: HttpException =>
         Left(UpstreamErrorResponse(exception.message, BAD_GATEWAY, BAD_GATEWAY))
       }
     )
