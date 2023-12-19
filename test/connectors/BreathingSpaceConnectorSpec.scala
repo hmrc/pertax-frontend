ba5e5f2e8850a8375ec0e26366122626bd256bad
grantrobson <grant.robson@digital.hmrc.gov.uk>
2023-12-19 13:18:03 +0000

    8320 Tidy

--- test/connectors/BreathingSpaceConnectorSpec.scala
+++ test/connectors/BreathingSpaceConnectorSpec.scala
@@ -19,9 +19,7 @@ package connectors
 import com.github.tomakehurst.wiremock.client.WireMock._
 import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder
 import config.ConfigDecorator
-import org.mockito.Mockito.{doNothing, when}
-import org.mockito.{ArgumentMatchers, Mockito}
-import org.mockito.MockitoSugar.{mock, times}
+import org.mockito.{ArgumentMatchers, Mockito, MockitoSugar}
 import play.api.{Application, Logger}
 import testUtils.WireMockHelper
 import uk.gov.hmrc.domain.{Generator, Nino}
@@ -30,7 +28,7 @@ import uk.gov.hmrc.http.{HttpResponse, UpstreamErrorResponse}
 
 import scala.util.Random
 
-class BreathingSpaceConnectorSpec extends ConnectorSpec with WireMockHelper {
+class BreathingSpaceConnectorSpec extends ConnectorSpec with WireMockHelper with MockitoSugar {
 
   override implicit lazy val app: Application = app(
     Map("microservice.services.breathing-space-if-proxy.port" -> server.port())
@@ -65,97 +63,82 @@ class BreathingSpaceConnectorSpec extends ConnectorSpec with WireMockHelper {
   private def httpClientV2: HttpClientV2       = app.injector.instanceOf[HttpClientV2]
   private def configDecorator: ConfigDecorator = app.injector.instanceOf[ConfigDecorator]
 
-  override def beforeEach(): Unit =
-    super.beforeEach()
-  //  Mockito.reset(mockLogger)
+  private val mockLogger: Logger = mock[Logger]
+
+  def httpClientResponseUsingMockLogger: HttpClientResponse = new HttpClientResponse {
+    override protected val logger: Logger = mockLogger
+  }
 
   "getBreathingSpaceIndicator is called" must {
-//    "return a true right response" in {
-//      stubGet(url, OK, Some(breathingSpaceTrueResponse))
-//
-//      val result = connector.getBreathingSpaceIndicator(nino).value.futureValue
-//      result mustBe a[Right[_, _]]
-//      result.getOrElse(HttpResponse(OK, "")) mustBe true
-//      verifyHeader(getRequestedFor(urlEqualTo(url)))
-//    }
-//
-//    "return a false right response" in {
-//      stubGet(url, OK, Some(breathingSpaceFalseResponse))
-//
-//      val result = connector.getBreathingSpaceIndicator(nino).value.futureValue
-//      result mustBe a[Right[_, _]]
-//      result.getOrElse(HttpResponse(OK, "")) mustBe false
-//      verifyHeader(getRequestedFor(urlEqualTo(url)))
-//    }
-//
-//    "return an UpstreamErrorResponse for timeout response" in {
-//      val delay: Int = 6000
-//      stubWithDelay(url, OK, None, Some(breathingSpaceTrueResponse), delay)
-//
-//      val result = connector.getBreathingSpaceIndicator(nino).value.futureValue
-//      result mustBe a[Left[UpstreamErrorResponse, _]]
-//      verifyHeader(getRequestedFor(urlEqualTo(url)))
-//    }
-//
-//    List(
-//      TOO_MANY_REQUESTS,
-//      INTERNAL_SERVER_ERROR,
-//      BAD_GATEWAY,
-//      SERVICE_UNAVAILABLE,
-//      IM_A_TEAPOT,
-//      NOT_FOUND,
-//      BAD_REQUEST,
-//      UNPROCESSABLE_ENTITY
-//    ).foreach { httpResponse =>
-//      s"return an UpstreamErrorResponse when $httpResponse status is received" in {
-//        stubGet(url, httpResponse, None)
-//
-//        val result = connector.getBreathingSpaceIndicator(nino).value.futureValue
-//        result mustBe a[Left[UpstreamErrorResponse, _]]
-//        verifyHeader(getRequestedFor(urlEqualTo(url)))
-//      }
-//    }
+    "return a true right response" in {
+      stubGet(url, OK, Some(breathingSpaceTrueResponse))
 
-    "return Left but 0 warnings logged & 1 error if receive 400" in {
+      val result = connector.getBreathingSpaceIndicator(nino).value.futureValue
+      result mustBe a[Right[_, _]]
+      result.getOrElse(HttpResponse(OK, "")) mustBe true
+      verifyHeader(getRequestedFor(urlEqualTo(url)))
+    }
 
-      val mockLogger: Logger = mock[Logger]
+    "return a false right response" in {
+      stubGet(url, OK, Some(breathingSpaceFalseResponse))
 
-      def httpClientResponseUsingMockLogger: HttpClientResponse = new HttpClientResponse {
-        override protected val logger: Logger = mockLogger
-      }
+      val result = connector.getBreathingSpaceIndicator(nino).value.futureValue
+      result mustBe a[Right[_, _]]
+      result.getOrElse(HttpResponse(OK, "")) mustBe false
+      verifyHeader(getRequestedFor(urlEqualTo(url)))
+    }
 
-      val connector = new BreathingSpaceConnector(httpClientV2, httpClientResponseUsingMockLogger, configDecorator) {}
-      doNothing().when(mockLogger).warn(ArgumentMatchers.any())(ArgumentMatchers.any())
-      doNothing().when(mockLogger).error(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any())
-
-      stubGet(url, BAD_REQUEST, Some("bad request"))
+    "return an UpstreamErrorResponse for timeout response" in {
+      val delay: Int = 6000
+      stubWithDelay(url, OK, None, Some(breathingSpaceTrueResponse), delay)
 
       val result = connector.getBreathingSpaceIndicator(nino).value.futureValue
       result mustBe a[Left[UpstreamErrorResponse, _]]
-      result.swap.exists(_.statusCode == BAD_REQUEST)
       verifyHeader(getRequestedFor(urlEqualTo(url)))
+    }
 
-      Mockito
-        .verify(mockLogger, times(0))
-        .warn(ArgumentMatchers.any())(ArgumentMatchers.any())
-      Mockito
-        .verify(mockLogger, times(1))
-        .error(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any())
+    List(
+      TOO_MANY_REQUESTS,
+      INTERNAL_SERVER_ERROR,
+      BAD_GATEWAY,
+      SERVICE_UNAVAILABLE,
+      IM_A_TEAPOT,
+      NOT_FOUND,
+      BAD_REQUEST,
+      UNPROCESSABLE_ENTITY
+    ).foreach { httpResponse =>
+      s"return an UpstreamErrorResponse when $httpResponse status is received and log 0 warnings & 1 error" in {
+        reset(mockLogger)
+        val connector = new BreathingSpaceConnector(httpClientV2, httpClientResponseUsingMockLogger, configDecorator) {}
+        doNothing.when(mockLogger).warn(ArgumentMatchers.any())(ArgumentMatchers.any())
+        doNothing.when(mockLogger).error(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any())
+        stubGet(url, httpResponse, None)
+
+        val result = connector.getBreathingSpaceIndicator(nino).value.futureValue
+        result mustBe a[Left[UpstreamErrorResponse, _]]
+        verifyHeader(getRequestedFor(urlEqualTo(url)))
+        Mockito
+          .verify(mockLogger, times(0))
+          .warn(ArgumentMatchers.any())(ArgumentMatchers.any())
+        Mockito
+          .verify(mockLogger, times(1))
+          .error(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any())
+      }
     }
 
     "return Left but 1 warning logged & no errors if receive 401 (indicates IF being restarted due to new release)" in {
-      val mockLogger: Logger = mock[Logger]
+      reset(mockLogger)
 
-      def httpClientResponseUsingMockLogger: HttpClientResponse = new HttpClientResponse {
-        override protected val logger: Logger = mockLogger
-      }
-      val connector                                             = new BreathingSpaceConnector(httpClientV2, httpClientResponseUsingMockLogger, configDecorator) {}
-      doNothing().when(mockLogger).warn(ArgumentMatchers.any())(ArgumentMatchers.any())
-      doNothing().when(mockLogger).error(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any())
+      val connector = new BreathingSpaceConnector(httpClientV2, httpClientResponseUsingMockLogger, configDecorator) {}
+      doNothing.when(mockLogger).warn(ArgumentMatchers.any())(ArgumentMatchers.any())
+      doNothing.when(mockLogger).error(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any())
 
-      val responseBody =
-        """{"errors":[{"code":"BREATHING_SPACE_EXPIRED","message":"Breathing Space has expired for the given Nino"}]}"""
-      stubGet(url, UNAUTHORIZED, Some(responseBody))
+      stubGet(
+        url,
+        UNAUTHORIZED,
+        Some("""{"errors":[{"code":"BREATHING_SPACE_EXPIRED","message":
+            |"Breathing Space has expired for the given Nino"}]}""".stripMargin)
+      )
 
       val result = connector.getBreathingSpaceIndicator(nino).value.futureValue
       result mustBe a[Left[UpstreamErrorResponse, _]]
