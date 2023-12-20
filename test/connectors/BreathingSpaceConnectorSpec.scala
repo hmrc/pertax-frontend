84061bf935bf5586163b9be88f88458f57376880
grantrobson <grant.robson@digital.hmrc.gov.uk>
2023-12-20 09:28:49 +0000

    8320 Correct logging test for not found

--- test/connectors/BreathingSpaceConnectorSpec.scala
+++ test/connectors/BreathingSpaceConnectorSpec.scala
@@ -100,6 +100,30 @@ class BreathingSpaceConnectorSpec extends ConnectorSpec with WireMockHelper with
     }
 
     List(
+      NOT_FOUND
+    ).foreach { httpResponse =>
+      s"return an UpstreamErrorResponse when $httpResponse status is received and log 0 warnings & 1 info" in {
+        reset(mockLogger)
+        val connector = new BreathingSpaceConnector(httpClientV2, httpClientResponseUsingMockLogger, configDecorator) {}
+        doNothing.when(mockLogger).warn(ArgumentMatchers.any())(ArgumentMatchers.any())
+        doNothing.when(mockLogger).error(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any())
+        stubGet(url, httpResponse, Some(dummyContent))
+
+        val result                              = connector.getBreathingSpaceIndicator(nino).value.futureValue
+        result mustBe a[Left[UpstreamErrorResponse, _]]
+        verifyHeader(getRequestedFor(urlEqualTo(url)))
+        Mockito
+          .verify(mockLogger, times(0))
+          .warn(ArgumentMatchers.any())(ArgumentMatchers.any())
+        val eventCaptor: ArgumentCaptor[String] = ArgumentCaptor.forClass(classOf[String])
+        Mockito
+          .verify(mockLogger, times(1))
+          .info(eventCaptor.capture())(ArgumentMatchers.any())
+        eventCaptor.getValue.contains(dummyContent) mustBe true
+      }
+    }
+
+    List(
       TOO_MANY_REQUESTS,
       INTERNAL_SERVER_ERROR,
       BAD_GATEWAY,
@@ -128,7 +152,6 @@ class BreathingSpaceConnectorSpec extends ConnectorSpec with WireMockHelper with
 
     List(
       IM_A_TEAPOT,
-      NOT_FOUND,
       BAD_REQUEST,
       UNPROCESSABLE_ENTITY
     ).foreach { httpResponse =>
