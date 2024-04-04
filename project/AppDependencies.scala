50bcd03f9128d1b884aee190268620524180f144
askar.makkunnath <askar.makkunnath@digital.hmrc.gov.uk>
2024-04-04 11:46:21 +0100

    DDCNL-8460: Upgraded libraries and plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,32 +1,31 @@
-import play.sbt.PlayImport._
-import sbt._
+import sbt.*
+import play.sbt.PlayImport.*
 
 object AppDependencies {
 
-  private val playVersion      = "play-28"
-  private val hmrcMongoVersion = "1.3.0"
-  private val bootstrapVersion = "8.1.0"
+  private val playVersion      = "play-30"
+  private val hmrcMongoVersion = "1.8.0"
+  private val bootstrapVersion = "8.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"       %% s"http-caching-client-$playVersion"  % s"11.0.0",
+    "uk.gov.hmrc"       %% s"http-caching-client-$playVersion"  % "11.2.0",
     "uk.gov.hmrc"       %% "tax-year"                           % "4.0.0",
     "io.lemonlabs"      %% "scala-uri"                          % "4.0.3",
-    "org.jsoup"          % "jsoup"                              % "1.16.1",
+    "org.jsoup"          % "jsoup"                              % "1.17.2",
     "org.typelevel"     %% "cats-core"                          % "2.10.0",
-    "org.apache.commons" % "commons-text"                       % "1.10.0",
+    "org.apache.commons" % "commons-text"                       % "1.11.0",
     "uk.gov.hmrc"       %% s"sca-wrapper-$playVersion"          % "1.6.0",
-    "uk.gov.hmrc"       %% "mongo-feature-toggles-client"       % "0.3.0",
-    ehcache
+    "uk.gov.hmrc"       %% s"mongo-feature-toggles-client-$playVersion" % "1.3.0",
+    caffeine
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"         %% s"bootstrap-test-$playVersion"  % bootstrapVersion,
-    "org.mockito"         %% "mockito-scala-scalatest"       % "1.17.27",
-    "org.scalatestplus"   %% "scalacheck-1-17"               % "3.2.17.0",
-    "com.vladsch.flexmark" % "flexmark-all"                  % "0.62.2",
+    "org.mockito"         %% "mockito-scala-scalatest"       % "1.17.31",
+    "org.scalatestplus"   %% "scalacheck-1-17"               % "3.2.18.0",
     "uk.gov.hmrc.mongo"   %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion
-  ).map(_ % "test,it")
+  ).map(_ % "test")
 
   val all: Seq[ModuleID]  = compile ++ test
 }
