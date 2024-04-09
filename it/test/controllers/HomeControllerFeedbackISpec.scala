3c1847519976d2047c69379bd97f4fd141e755de
askar.makkunnath <askar.makkunnath@digital.hmrc.gov.uk>
2024-04-09 16:05:18 +0100

    DDCNL-8460: Moved IT to test folder

--- it/test/controllers/HomeControllerFeedbackISpec.scala
+++ it/test/controllers/HomeControllerFeedbackISpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import models.admin.SingleAccountCheckToggle
@@ -5,7 +21,6 @@ import org.mockito.ArgumentMatchers
 import org.mockito.Mockito.when
 import play.api.Application
 import play.api.http.Status._
-import play.api.i18n.Messages
 import play.api.mvc.{AnyContentAsEmpty, Result}
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{GET, contentAsString, defaultAwaitTimeout, route, writeableOf_AnyContentAsEmpty, status => httpStatus}
@@ -16,7 +31,7 @@ import uk.gov.hmrc.mongoFeatureToggles.model.FeatureFlag
 import java.util.UUID
 import scala.concurrent.Future
 
-class HomeControllerPayeISpec extends IntegrationSpec {
+class HomeControllerFeedbackISpec extends IntegrationSpec {
 
   override implicit lazy val app: Application = localGuiceApplicationBuilder()
     .configure(
@@ -36,16 +51,16 @@ class HomeControllerPayeISpec extends IntegrationSpec {
   override def beforeEach(): Unit = {
     super.beforeEach()
     beforeEachHomeController()
+
     when(mockFeatureFlagService.get(ArgumentMatchers.eq(SingleAccountCheckToggle)))
       .thenReturn(Future.successful(FeatureFlag(SingleAccountCheckToggle, isEnabled = true)))
   }
 
   "personal-account" must {
-    "show PAYE tile with the correct link for a user with a valid NINO" in {
+    "show the correct feedback link" in {
       val result: Future[Result] = route(app, request).get
       httpStatus(result) mustBe OK
-      contentAsString(result).contains(Messages("label.pay_as_you_earn_paye")) mustBe true
-      contentAsString(result).contains("/check-income-tax/what-do-you-want-to-do") mustBe true
+      contentAsString(result).contains("/contact/beta-feedback") mustBe true
     }
   }
 }
