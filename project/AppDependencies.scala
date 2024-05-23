bdc897e467ff6a8ba25f95f963bb4880efd421c7
vfryer12 <72739659+vfryer12@users.noreply.github.com>
2024-05-23 12:35:36 +0100

    [DDCNL-8985]Updated sca-wrapper and bootstrap

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
 
   private val playVersion      = "play-30"
   private val hmrcMongoVersion = "1.8.0"
-  private val bootstrapVersion = "8.5.0"
+  private val bootstrapVersion = "8.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
@@ -15,7 +15,7 @@ object AppDependencies {
     "org.jsoup"          % "jsoup"                                      % "1.17.2",
     "org.typelevel"     %% "cats-core"                                  % "2.10.0",
     "org.apache.commons" % "commons-text"                               % "1.11.0",
-    "uk.gov.hmrc"       %% s"sca-wrapper-$playVersion"                  % "1.6.0",
+    "uk.gov.hmrc"       %% s"sca-wrapper-$playVersion"                  % "1.8.0",
     "uk.gov.hmrc"       %% s"mongo-feature-toggles-client-$playVersion" % "1.3.0"
   )
 
