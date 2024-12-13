8aa806028f9b6252ab91781cac8ed67556fbb7d9
askar.makkunnath <askar.makkunnath@digital.hmrc.gov.uk>
2024-12-13 11:13:22 +0000

    DDCNL-9642: Removed ItsAdvertisementMessageToggle

--- app/controllers/InterstitialController.scala
+++ app/controllers/InterstitialController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2024 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import controllers.auth.requests.UserRequest
 import controllers.auth.{AuthJourney, WithBreadcrumbAction}
 import error.ErrorRenderer
 import models._
-import models.admin.{BreathingSpaceIndicatorToggle, ItsAdvertisementMessageToggle, ShowOutageBannerToggle}
+import models.admin.{BreathingSpaceIndicatorToggle, ShowOutageBannerToggle}
 import play.api.Logging
 import play.api.mvc._
 import play.twirl.api.Html
@@ -128,19 +128,17 @@ class InterstitialController @Inject() (
         .itsaEnrolmentStatus(request.enrolments)
         .isDefined && request.trustedHelper.isEmpty && request.isSa
     ) {
-      for {
-        hasSeissClaims    <- seissService.hasClaims(saUserType)
-        itsaMessageToggle <- featureFlagService.get(ItsAdvertisementMessageToggle)
-      } yield Ok(
-        viewItsaMergePageView(
-          nextDeadlineTaxYear = (current.currentYear + 1).toString,
-          request.isSa,
-          itsaMessageToggle.isEnabled,
-          hasSeissClaims,
-          taxYear = previousAndCurrentTaxYear,
-          saUserType
+      seissService.hasClaims(saUserType).map { hasSeissClaims =>
+        Ok(
+          viewItsaMergePageView(
+            nextDeadlineTaxYear = (current.currentYear + 1).toString,
+            request.isSa,
+            hasSeissClaims,
+            taxYear = previousAndCurrentTaxYear,
+            saUserType
+          )
         )
-      )
+      }
     } else {
       errorRenderer.futureError(UNAUTHORIZED)
     }
