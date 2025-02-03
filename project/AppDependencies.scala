31f959824c7f2794b78a45c003bfa6397e154e98
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2025-02-03 14:42:07 +0000

    change webchat to pta home page only

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,6 @@ import sbt.*
 object AppDependencies {
 
   private val playVersion      = "play-30"
-  private val hmrcMongoVersion = "2.3.0"
-  private val bootstrapVersion = "9.5.0"
   private val cryptoVersion    = "8.1.0"
   private val webChatVersion   = "1.6.0"
 
@@ -13,20 +11,20 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc"       %% "tax-year"                                   % "5.0.0",
     "io.lemonlabs"      %% "scala-uri"                                  % "4.0.3",
-    "org.jsoup"          % "jsoup"                                      % "1.18.1",
-    "org.typelevel"     %% "cats-core"                                  % "2.12.0",
+    "org.jsoup"          % "jsoup"                                      % "1.18.3",
+    "org.typelevel"     %% "cats-core"                                  % "2.13.0",
     "org.apache.commons" % "commons-text"                               % "1.12.0",
     "uk.gov.hmrc"       %% s"sca-wrapper-$playVersion"                  % "2.1.0",
-    "uk.gov.hmrc"       %% s"mongo-feature-toggles-client-$playVersion" % "1.8.0",
+    "uk.gov.hmrc"       %% s"mongo-feature-toggles-client-$playVersion" % "1.9.0",
     "uk.gov.hmrc"       %% s"crypto-json-$playVersion"                  % cryptoVersion,
     "uk.gov.hmrc"       %% "digital-engagement-platform-chat-30"        % webChatVersion
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"         %% s"bootstrap-test-$playVersion"  % bootstrapVersion,
-    "org.mockito"         %% "mockito-scala-scalatest"       % "1.17.37",
-    "org.scalatestplus"   %% "scalacheck-1-17"               % "3.2.18.0",
-    "uk.gov.hmrc.mongo"   %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion
+    "uk.gov.hmrc"         %% s"sca-wrapper-test-$playVersion" % "2.1.0",
+    "uk.gov.hmrc"         %% s"mongo-feature-toggles-client-test-$playVersion" % "1.9.0",
+    "org.mockito"         %% "mockito-scala-scalatest"        % "1.17.37",
+    "org.scalatestplus"   %% "scalacheck-1-17"                % "3.2.18.0",
   ).map(_ % "test")
 
   val all: Seq[ModuleID]  = compile ++ test
