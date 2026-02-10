47be00af2115ecb0ec1ae40ddcc83b8e1395976b
klawani <456043+klawani@users.noreply.github.com>
2026-02-10 11:57:47 +0000

    [DDCNL-11482]: add ID to taxes and benefits H2 element of the newHomePageDesign

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -23,6 +23,7 @@ import controllers.controllershelpers.{HomeCardGenerator, HomeOptionsGenerator, 
 import models.BreathingSpaceIndicatorResponse
 import models.BreathingSpaceIndicatorResponse.WithinPeriod
 import models.admin.{GetPersonFromCitizenDetailsToggle, HomePageNewLayoutToggle, ShowPlannedOutageBannerToggle}
+import org.jsoup.Jsoup
 import org.mockito.ArgumentMatchers
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.{reset, times, verify, when}
@@ -321,15 +322,18 @@ class HomeControllerSpec extends BaseSpec with WireMockHelper {
       when(mockFeatureFlagService.get(HomePageNewLayoutToggle))
         .thenReturn(Future.successful(FeatureFlag(HomePageNewLayoutToggle, isEnabled = true)))
 
-      val newLayoutHtmlString   = "<h2class=\"govuk-heading-m\">Taxesandbenefits</h2>"
       val appLocal: Application = appBuilder.build()
 
-      val controller: HomeController  = appLocal.injector.instanceOf[HomeController]
-      val result: Future[Result]      = controller.index()(newDesignRequest)
+      val controller: HomeController = appLocal.injector.instanceOf[HomeController]
+      val result: Future[Result]     = controller.index()(newDesignRequest)
       status(result) mustBe OK
-      val containsNewLayoutHtmlString =
-        contentAsString(result).replaceAll("\\s", "").contains(newLayoutHtmlString.replaceAll("\\s", ""))
-      assert(containsNewLayoutHtmlString)
+
+      val htmlContent = contentAsString(result)
+      htmlContent must include("""id="taxes-and-benefits-heading""")
+
+      val content                 = Jsoup.parse(htmlContent)
+      val taxesAndBenefitsElement = content.getElementById("taxes-and-benefits-heading")
+      taxesAndBenefitsElement must not be null
     }
 
     "Return the old home page design when newDesign parameter is false " in {
