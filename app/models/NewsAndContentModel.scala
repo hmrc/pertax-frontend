e7a5ce930d8fb95388fe66661d859e0eeb8c86cb
askar.makkunnath <askar.makkunnath@digital.hmrc.gov.uk>
2026-04-24 16:50:36 +0100

    DDCNL-11853: Add flag to pin a news item to top of list

--- app/models/NewsAndContentModel.scala
+++ app/models/NewsAndContentModel.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,5 +24,6 @@ case class NewsAndContentModel(
   content: String,
   isDynamic: Boolean,
   startDate: LocalDate,
-  toBeDisplayed: Boolean
+  toBeDisplayed: Boolean,
+  pinToTop: Boolean = false
 )
