8b35973ca9593a1997d0f37b0826bf8f58fe304d
Olakunle Kuye <olakunlekuye@klxsolutions.co.uk>
2026-05-19 14:09:18 +0100

    PTAD-104 Add reusable card components

---
+++ app/viewmodels/CardContainerModel.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels
+
+import play.twirl.api.Html
+
+final case class CardContainerModel(
+  emptyView: Html,
+  header: Option[String] = None,
+  cards: Seq[HMRCCardModel] = Seq.empty,
+  headingLevel: String = "h2",
+  listAriaLabel: Option[String] = None,
+  headerId: Option[String] = None
+) {
+  val normalizedHeadingLevel: String = headingLevel.trim.toLowerCase
+  val normalizedHeader: Option[String] = header.map(_.trim).filter(_.nonEmpty)
+  val normalizedListAriaLabel: Option[String] = listAriaLabel.map(_.trim).filter(_.nonEmpty)
+  val normalizedHeaderId: Option[String] = headerId.map(_.trim).filter(_.nonEmpty)
+
+  require(
+    CardContainerModel.ValidHeadingLevels.contains(normalizedHeadingLevel),
+    s"Invalid heading level: $headingLevel. Must be h1-h6."
+  )
+
+  require(
+    cards.length < 2 || normalizedHeader.isDefined || normalizedListAriaLabel.isDefined,
+    "CardContainerModel with 2+ cards must have either header or listAriaLabel for accessibility"
+  )
+}
+
+object CardContainerModel {
+  private val ValidHeadingLevels = Set("h1", "h2", "h3", "h4", "h5", "h6")
+}
---
+++ test/views/html/tags/CardContainerSpec.scala
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.html.tags
+
+import play.twirl.api.Html
+import views.html.ViewSpec
+import viewmodels.{CardContainerModel, HMRCCardModel}
+
+class CardContainerSpec extends ViewSpec {
+
+  lazy val cardContainer = inject[views.html.tags.CardContainer]
+
+  "CardContainer" must {
+
+    "render emptyView when cards is empty" in {
+      val model = CardContainerModel(
+        emptyView = Html("<p>No cards</p>"),
+        header = Some("Test Header"),
+        cards = Seq.empty
+      )
+      val doc = asDocument(cardContainer(model).toString)
+      doc.text() must include("No cards")
+      doc.select("ul").size() mustBe 0
+    }
+
+    "render single card without ul wrapper" in {
+      val model = CardContainerModel(
+        emptyView = Html(""),
+        cards = Seq(HMRCCardModel(Html("<div class=\"card\">Card 1</div>")))
+      )
+      val doc = asDocument(cardContainer(model).toString)
+      doc.select("ul").size() mustBe 0
+      doc.select(".card").size() mustBe 1
+    }
+
+    "render multiple cards in ul with li wrappers" in {
+      val model = CardContainerModel(
+        emptyView = Html(""),
+        cards = Seq(
+          HMRCCardModel(Html("<div class=\"card\">Card 1</div>")),
+          HMRCCardModel(Html("<div class=\"card\">Card 2</div>"))
+        ),
+        header = Some("Test Header"),
+        headerId = Some("test-container")
+      )
+      val doc = asDocument(cardContainer(model).toString)
+      doc.select("ul.card-container-list").size() mustBe 1
+      doc.select("ul.card-container-list > li").size() mustBe 2
+    }
+
+    "use aria-label from listAriaLabel when provided (precedence over header)" in {
+      val model = CardContainerModel(
+        emptyView = Html(""),
+        header = Some("Test Header"),
+        listAriaLabel = Some("Custom label"),
+        cards = Seq(
+          HMRCCardModel(Html("<div>Card</div>")),
+          HMRCCardModel(Html("<div>Card</div>"))
+        )
+      )
+      val doc = asDocument(cardContainer(model).toString)
+      doc.select("ul").attr("aria-label") mustBe "Custom label"
+      doc.select("ul").hasAttr("aria-labelledby") mustBe false
+    }
+
+    "use aria-labelledby from header when listAriaLabel not provided" in {
+      val model = CardContainerModel(
+        emptyView = Html(""),
+        header = Some("Test Header"),
+        cards = Seq(
+          HMRCCardModel(Html("<div>Card</div>")),
+          HMRCCardModel(Html("<div>Card</div>"))
+        )
+      )
+      val doc = asDocument(cardContainer(model).toString)
+      doc.select("ul").attr("aria-labelledby") mustBe "card-container-header"
+      doc.select("ul").hasAttr("aria-label") mustBe false
+    }
+
+    "use custom headerId for both heading id and aria-labelledby" in {
+      val model = CardContainerModel(
+        emptyView = Html(""),
+        header = Some("Test Header"),
+        headerId = Some("custom-container-id"),
+        cards = Seq(
+          HMRCCardModel(Html("<div>Card</div>")),
+          HMRCCardModel(Html("<div>Card</div>"))
+        )
+      )
+      val doc = asDocument(cardContainer(model).toString)
+      doc.select("h2").attr("id") mustBe "custom-container-id"
+      doc.select("ul").attr("aria-labelledby") mustBe "custom-container-id"
+    }
+
+    "ignore empty listAriaLabel and use aria-labelledby from header" in {
+      val model = CardContainerModel(
+        emptyView = Html(""),
+        header = Some("Test Header"),
+        listAriaLabel = Some("   "),  // Whitespace only
+        cards = Seq(
+          HMRCCardModel(Html("<div>Card</div>")),
+          HMRCCardModel(Html("<div>Card</div>"))
+        )
+      )
+      val doc = asDocument(cardContainer(model).toString)
+      doc.select("ul").attr("aria-labelledby") mustBe "card-container-header"
+      doc.select("ul").hasAttr("aria-label") mustBe false
+    }
+
+    "ignore empty headerId and use default" in {
+      val model = CardContainerModel(
+        emptyView = Html(""),
+        header = Some("Test Header"),
+        headerId = Some("   "),  // Whitespace only
+        cards = Seq(
+          HMRCCardModel(Html("<div>Card</div>")),
+          HMRCCardModel(Html("<div>Card</div>"))
+        )
+      )
+      val doc = asDocument(cardContainer(model).toString)
+      doc.select("h2").attr("id") mustBe "card-container-header"
+      doc.select("ul").attr("aria-labelledby") mustBe "card-container-header"
+    }
+
+    "ignore empty header and not render heading" in {
+      val model = CardContainerModel(
+        emptyView = Html(""),
+        header = Some("   "),  // Whitespace only
+        cards = Seq(HMRCCardModel(Html("<div>Card</div>")))
+      )
+      val doc = asDocument(cardContainer(model).toString)
+      doc.select("h2").size() mustBe 0
+    }
+
+    "render configured heading level" in {
+      val model = CardContainerModel(
+        emptyView = Html(""),
+        header = Some("Test Header"),
+        headingLevel = " H3 ",
+        cards = Seq(HMRCCardModel(Html("<div>Card</div>")))
+      )
+      val doc = asDocument(cardContainer(model).toString)
+      doc.select("h3").text() mustBe "Test Header"
+    }
+
+    "throw error for invalid heading level" in {
+      an[IllegalArgumentException] must be thrownBy {
+        CardContainerModel(
+          emptyView = Html(""),
+          headingLevel = "h7",
+          cards = Seq(HMRCCardModel(Html("<div>Card</div>")))
+        )
+      }
+    }
+
+    "throw error when cards >= 2 and no accessible name" in {
+      an[IllegalArgumentException] must be thrownBy {
+        CardContainerModel(
+          emptyView = Html(""),
+          cards = Seq(
+            HMRCCardModel(Html("<div>Card</div>")),
+            HMRCCardModel(Html("<div>Card</div>"))
+          )
+        )
+      }
+    }
+  }
+}
