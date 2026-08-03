a14a80eab222789df44e54c17a0731ab433a2c7c
Olakunle Kuye <olakunlekuye@klxsolutions.co.uk>
2026-08-03 15:13:56 +0100

    fix: update HomeControllerSpec mocks for isRedesign parameter
    - Update mock setup to include the new isRedesign parameter in getHomePageServices() calls
    - Fixes Mockito matcher count mismatch (4 expected, 3 recorded)

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -144,7 +144,7 @@ class HomeControllerSpec extends BaseSpec with WireMockHelper with CitizenDetail
     when(mockConfigDecorator.ptapHomepageNinoRolloutLastNumericDigits)
       .thenReturn(Seq(0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
 
-    when(mockHomePageServicesProvider.getHomePageServices()(any(), any(), any()))
+    when(mockHomePageServicesProvider.getHomePageServices(any())(any(), any(), any()))
       .thenReturn(Future.successful(HomePageServices(Seq.empty)))
 
     when(mockCitizenDetailsService.personDetails(any(), any())(any(), any(), any()))
@@ -667,7 +667,7 @@ class HomeControllerSpec extends BaseSpec with WireMockHelper with CitizenDetail
         hintText = Some("Child Benefit hint")
       )
 
-      when(mockHomePageServicesProvider.getHomePageServices()(any(), any(), any()))
+      when(mockHomePageServicesProvider.getHomePageServices(any())(any(), any(), any()))
         .thenReturn(Future.successful(HomePageServices(Seq(payeService, childBenefitService))))
 
       val appLocal   = appBuilder.build()
