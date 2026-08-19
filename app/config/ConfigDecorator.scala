1b672cad0ffa86cebc104c08fad2ed08bf812c54
Olakunle Kuye <olakunlekuye@klxsolutions.co.uk>
2026-08-19 17:26:30 +0100

    PTAD-239 : Removed commented code

--- app/config/ConfigDecorator.scala
+++ app/config/ConfigDecorator.scala
@@ -56,10 +56,6 @@ class ConfigDecorator @Inject() (
   private lazy val formFrontendService     = servicesConfig.baseUrl("dfs-digital-forms-frontend")
   private lazy val taxCalcFrontendService  = servicesConfig.baseUrl("taxcalc-frontend")
   private lazy val taxCalcFrontendExternal = getExternalUrl("taxcalc-frontend.host").getOrElse("")
-  //private lazy val leppFrontendService     = servicesConfig.baseUrl("low-earners-pensions-payment-frontend")
-  // Use if taking to a local service
-  //= "http://localhost:9232/personal-account"
-  //IF using staging ptaUrl                    = "/personal-account"
 
 
   lazy val businessTaxAccountService: String = servicesConfig.baseUrl("business-tax-account")
